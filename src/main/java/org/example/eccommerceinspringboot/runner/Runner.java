5b27dd858aae4f89bba726abf788a911c8b772ac
Sanjar Ismailov <sanjar2008gmail.com>
2024-08-27 17:49:32 +0500

    ok

--- src/main/java/org/example/eccommerceinspringboot/runner/Runner.java
+++ src/main/java/org/example/eccommerceinspringboot/runner/Runner.java
@@ -21,14 +21,7 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception{
         if (ddt.equals("create")){
-            for (int i = 1; i <=10 ; i++) {
-                userRepo.save(new User(
-                        i,
-                        "user"+i,
-                        passwordEncoder.encode("p"+i),
-                        "UserUsers"+i
-                ));
-            }
+            userRepo.save(new User(1, "sanjar", passwordEncoder.encode("2008"), passwordEncoder.encode("Sanjar Ismailov")));
         }
     }
 }
